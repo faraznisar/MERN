e2c5910760528bee7be0fbd3e974f00839ac6840
Shah <fshah7@dxc.com>
2020-01-31 19:15:50 +0530

    Backdrop for sidedrawer- close and open

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import MainHeader from './MainHeader'
 import SideDrawer from './SideDrawer'
 import NavLinks from './NavLinks'
+import Backdrop from '../UIElements/Backdrop'
 import './MainNavigation.css'
 
 const MainNavigation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true)
+    }
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false)
+    }
+
     return (
         <React.Fragment>
-            <SideDrawer>
+            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
                 <NavLinks />
             </SideDrawer>
+
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
                     <span />
                     <span />
                     <span />
